f009326fa39018fc347ed9cce6f57674a2cacd61
Luis Carlos Villa <luis.villa@varangard.com>
2018-12-28 12:31:12 -0600

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,21 @@
+//=============
+//Puerto
+//=============
+process.env.PORT = process.env.PORT || 3000;
+
+//=============
+//Entorno
+//=============
+process.env.NODE_ENV = process.env.NODE_ENV	|| 'dev';
+
+//=============
+//Base de datos
+//=============
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev' ){
+	urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+	urlDB = 'mongodb://cafe-user:1234qwer@ds111336.mlab.com:11336/cafe'
+}
+process.env.URLDB = urlDB;
